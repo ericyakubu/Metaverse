dd8b67586d08c118f06a94fa34d458d0189db3fe
CaxaROCK <yakubueric@gmail.com>
2023-12-06 09:31:57 +0400

    font change

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,15 @@ const MyApp = ({ Component, pageProps }) => (
       <title>Metaversus</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://stijndv.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
-        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
       />
     </Head>
     <Component {...pageProps} />
